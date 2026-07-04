a7f9259f4e20e642a428ae275ddb2219c85b11f5
Fred <fred.veyrieres@gmail.com>
2026-07-04 11:30:00 +0200

    Ajout filtre tous

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -6,21 +6,22 @@ fetch("http://localhost:5678/api/works")
     .then(works => {
 
         works.forEach(project => {
-
             const figure = document.createElement("figure");
-
             const image = document.createElement("img");
             image.src = project.imageUrl;
             image.alt = project.title;
-
             const figcaption = document.createElement("figcaption");
             figcaption.textContent = project.title;
-
             figure.appendChild(image);
             figure.appendChild(figcaption);
-
             galerie.appendChild(figure);
-
         });
-
     });
+
+const filtres = document.querySelector(".filters");
+
+    const boutonTous = document.createElement("button");
+    boutonTous.textContent = "Tous";
+    filtres.appendChild(boutonTous);
+
+
